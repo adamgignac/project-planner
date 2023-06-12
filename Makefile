aab05286c25ded3ddc314bf51b745416b83c3e63
Adam Gignac <gignac.adam+github@gmail.com>
2023-06-11 21:49:44 -0400

    Add some tests for bin packing

--- Makefile
+++ Makefile
@@ -8,6 +8,10 @@ lint:
 	poetry run isort project_planner/
 	poetry run ruff --fix project_planner/
 
+.PHONY: test
+test:
+	poetry run python -m unittest
+
 .PHONY: run
 run:
 	poetry run python -m project_planner project.yaml
---
+++ tests/test_bin_packer.py
@@ -0,0 +1,31 @@
+from project_planner.bin_packer import Bin, BinPacker
+
+import unittest
+
+class TestBinPacker(unittest.TestCase):
+    def test_pack_item_does_not_fit(self):
+        p = BinPacker[int](10)
+        with self.assertRaises(ValueError):
+            p.pack([12])
+        
+    def test_pack_item_does_fit(self):
+        p = BinPacker[int](10)
+        packed = p.pack([8])
+        self.assertEqual(len(packed), 1)
+    
+    def test_pack_item_fits_exactly_without_padding(self):
+        p = BinPacker[int](10)
+        packed = p.pack([10])
+        self.assertEqual(len(packed), 1)
+    
+    def test_pack_item_fits_exactly_with_padding(self):
+        p = BinPacker[int](10, padding=1)
+        packed = p.pack([10])
+        self.assertEqual(len(packed), 1)
+    
+    def test_items_with_padding(self):
+        p = BinPacker[int](10, padding=1)
+        packed = p.pack([5, 5])
+        self.assertEqual(len(packed), 2)
+        packed = p.pack([6, 3])
+        self.assertEqual(len(packed), 1)
